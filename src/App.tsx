1795afcfcb47b681b6c9510e471d3d7fadfa9b59
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-11-02 12:03:44 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import Person from './Person';
+// import Person from './Person';
 
-const klaus: Person = {
-  id: 1,
-  firstName: 'Klaus',
-  lastName: 'Müller',
-  birthdate: '1970-01-01T01:00:00Z',
-  street: 'Main street 5',
-  city: 'Democity',
-  zipCode: '12345',
-};
+// const klaus: Person = {
+//   id: 1,
+//   firstName: 'Klaus',
+//   lastName: 'Müller',
+//   birthdate: '1970-01-01T01:00:00Z',
+//   street: 'Main street 5',
+//   city: 'Democity',
+//   zipCode: '12345',
+// };
 
 // export default function App() { ... }
 const App: React.FC = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -11,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <App />
+    <hr />
     <List />
   </React.StrictMode>
 );
