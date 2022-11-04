e65b9dadbfbc9a76be09329e9366961d80003fdf
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2022-11-04 09:39:27 +0100

    context

--- src/PersonProvider.tsx
+++ src/PersonProvider.tsx
@@ -9,11 +9,14 @@ import Person from './Person';
 
 type PersonContextType = [Person[], Dispatch<SetStateAction<Person[]>>] | null;
 
+// 1. create new Context
 const PersonContext = createContext<PersonContextType>(null);
 
 type Props = {
   children: React.ReactNode;
 };
+
+// 2. create new Provider component (with value)
 const PersonProvider: React.FC<Props> = ({ children }) => {
   const personState = useState<Person[]>([]);
   return (
@@ -25,6 +28,7 @@ const PersonProvider: React.FC<Props> = ({ children }) => {
 
 export default PersonProvider;
 
+// 3. access context via useContext (with the corresponding Context object)
 export function usePersonContext() {
   const personState = useContext(PersonContext);
 
